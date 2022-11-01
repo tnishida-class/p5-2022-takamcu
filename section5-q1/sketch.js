49daace4dc421fbffbfe65e4fd5ec372e96dff11
takamcu <takayuki.mcu@outlook.jp>
2022-11-01 12:27:32 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,22 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("With great power comes great responsibility.");
 }
 
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
+  let p = 4;
+  fill(255, 0, 0);
+  stroke(0)
+  rect(0, 0, w + p * 2, h + p * 4);
+  stroke(255, 0, 0)
+  triangle(w + p * 2, h + p * 4 - 10, w + p * 2 - 10, h + p * 4, w + p * 2 + 10, h  + p * 4 + 10)
+  stroke(0)
+  line(w + p * 2, h + p * 4 - 10, w + p * 2 + 10, h  + p * 4 + 10)
+  line(w + p * 2 - 10, h + p * 4, w + p * 2 + 10, h  + p * 4 + 10)
+  noStroke()
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
   text(t, p, h + p);
 }
